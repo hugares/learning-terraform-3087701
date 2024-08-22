d1533642ba6690f1cc7b4a016b98440a2ae69e54
Hugo Arès <hares@redhat.com>
2024-08-22 14:52:10 -0400

    fix variable desc

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "ami_filter" {
 }
 
 variable "environment {
-  description "Development Environment"
+  description = "Development Environment"
 
   type = object({
     name           = string
